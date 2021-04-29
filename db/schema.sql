e02afc0b015a67a0a2ee1b6736d610d0be8b4d1a
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-29 17:29:42 -0500

    adding in seed info

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE department (
 );
 
 
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL(10,2),
@@ -26,6 +26,6 @@ CREATE TABLE employee (
   role_id INT,
   manager_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (roles_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES employee(id) 
 );
---
+++ db/seeds.sql
@@ -0,0 +1,39 @@
+use employee_db;
+
+/*department inserts*/ 
+INSERT INTO department (department_name)
+VALUES 
+('Operations'),
+('Human Resources'),
+('Information Technology'),
+('Engineering');
+
+/*role inserts*/
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+('Cheif Executive Officer', 200000, 1),
+('Cheif Operating Officer',150000, 1),
+('Chief Engineer', 120000,4),
+('Junior Engineer', 80000, 4),
+('Health and Safety Specialist', 120000, 2),
+('Organizational Psychologist',90000,2),
+('Data Protection Officer', 100000, 3),
+('Intern', 50000, 1),
+('Business Strategist', 105000, 1),
+('Morale Specialist', 90000, 2),
+('Developer', 80000,3);
+
+/*employee inserts*/
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES ('Jean-Luc', 'Picard', 1, NULL),
+('William T.', 'Riker', 2, 1),
+('Geordi', 'LaForge', 3, 1),
+('Beverly', 'Crusher', 5, 1),
+('Deanna', 'Troi', 6, 1),
+('Miles', "O'Brien", 4, 3),
+('Wesley', 'Crusher', 8, 3),
+('Worf','Moghson', 7, 1),
+('Ro','Laren', 9, 2),
+('Guinan','Goldberg',10, 1),
+('Reginald','Barclay',11,3);
